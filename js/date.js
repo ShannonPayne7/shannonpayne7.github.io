14d6ac2dcabfc8e4b8b4aac6b95b95cca7622a18
ShannonPayne7 <pay20012@byui.edu>
2021-01-09 09:16:20 -0700

    ready to submit

---
+++ js/date.js
@@ -0,0 +1,9 @@
+
+//date last modified
+let updateDate = document.lastModified;
+document.getElementById("dateModified").innerHTML = "Last updated " + updateDate;
+
+//copyright current year
+let myDate = new Date();
+let currentYear = myDate.getFullYear();
+document.getElementById("demo").innerHTML = currentYear;
