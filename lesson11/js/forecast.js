1a29e980224be0c5f5ad0c24730b44d85f46b67d
ShannonPayne7 <pay20012@byui.edu>
2021-03-15 11:15:56 -0700

    files updated lesson 11

---
+++ lesson11/js/forecast.js
@@ -0,0 +1,57 @@
+//weather summary
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=70d5699f094618095ca99927b544bc2c";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    const current = jsObject.weather[0].main + " " + Math.round(jsObject.main.temp);
+    document.getElementById("current").textContent = current;
+    document.getElementById("temperature").textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById("low").textContent = Math.round(jsObject.main.temp_min);
+    document.getElementById("windchill").innerText = windChill(jsObject.main.temp_max, jsObject.wind.speed);
+    document.getElementById("humidity").textContent = jsObject.main.humidity;
+    document.getElementById("windspeed").textContent = Math.round(jsObject.wind.speed);
+});
+
+//compute windchill
+function windChill(tempF, speed) {
+    if (tempF > 50 || speed <= 3) {
+        return "n/a";
+    }
+    let calculate = 35.74 + (0.6215 * tempF) - (35.75 * (speed ** 0.16)) +
+        (0.4275 * tempF * (speed ** 0.16));
+    return calculate.toFixed(0);
+}
+
+//five day forecast
+const apiForecastURL =
+  "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=70d5699f094618095ca99927b544bc2c";
+fetch(apiForecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+
+    let forecast = document.getElementById("forecast");
+
+    const datafilter = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+
+    let i = 1;
+    datafilter.forEach(day => {
+        document.getElementById("day" + i).textContent = getDayOfWeek(day.dt);
+        let imagesrc = "https://openweathermap.org/img/wn/" + day.weather[0].icon + "@2x.png";
+        document.getElementById("icon" + i).setAttribute("src", imagesrc);
+        document.getElementById("icon" + i).setAttribute("alt", day.weather[0].description);
+        document.getElementById("temp" + i).textContent = Math.round(day.main.temp) + " ℉";
+        i++;
+    });
+   
+  });
+  
+function getDayOfWeek(dt) {
+    let days = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat']; 
+    const timestamp = dt * 1000;
+    let dtDate = new Date(timestamp);
+    return days[ dtDate.getDay() ]; 
+}
+
