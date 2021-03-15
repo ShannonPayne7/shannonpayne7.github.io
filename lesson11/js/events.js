434643a584996132e84d01bb70a86ff1dfd36567
ShannonPayne7 <pay20012@byui.edu>
2021-03-15 15:04:05 -0700

    link to portal

---
+++ lesson11/js/events.js
@@ -0,0 +1,29 @@
+const requestURL =
+  "https://byui-cit230.github.io/weather/data/towndata.json";
+
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const towns = jsonObject["towns"];
+    const events =  document.getElementById("events");
+    const city = events.innerHTML;
+    events.innerHTML = "Upcoming Events";
+    const datafilter = towns.filter(x => x.name == city);
+
+    datafilter.forEach(town => {
+        let eventList = document.createElement("ul");
+        
+        town.events.forEach(event => {
+            let lineEvent = document.createElement("li");
+            lineEvent.innerHTML = event;
+            eventList.appendChild(lineEvent);
+        });
+
+        events.appendChild(eventList);
+    });
+  });
+
+
