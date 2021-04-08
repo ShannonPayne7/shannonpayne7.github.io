adbb46dfac42f1cf35367c2437678653820f755a
ShannonPayne7 <pay20012@byui.edu>
2021-04-07 22:43:17 -0700

    html corrections

--- chamberOfCommerce/js/toggleCards.js
+++ chamberOfCommerce/js/toggleCards.js
@@ -3,7 +3,9 @@ function toggleCards() {
   
     if (document.getElementById("directoryCards").className == "change") {
         document.getElementById("gridButton").setAttribute('src', 'https://img.icons8.com/metro/26/000000/list.png');
+        document.getElementById("gridButton").setAttribute('alt', 'list view');
     } else {
         document.getElementById("gridButton").setAttribute('src', 'https://img.icons8.com/metro/26/000000/activity-grid-2.png');
+        document.getElementById("gridButton").setAttribute('alt', 'grid view');
     }
   }
